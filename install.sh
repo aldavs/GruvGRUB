811aa42af0275cc215346f75db018d6d6b5dc207
aldavs <97813033+aldavs@users.noreply.github.com>
2024-01-09 22:41:40 -0500

    Add successful install message and return status

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 ### GruvGRUB: Install Script
 
 [ ! -f "GruvGRUB" ] && echo "Error: GruvGRUB not found." && return 1
-sudo chown -R root:root GruvGRUB
-sudo mkdir -p /boot/grub/themes/GruvGRUB/ && sudo mv GruvGRUB /boot/grub/themes/GruvGRUB/
-sudo grub-mkfont --verbose --range=0x0-0x7F --size=8 --output=/boot/grub/fonts/iosevka-8.pf2 /usr/share/fonts/truetype/iosevka/IosevkaNerdFont-Regular.ttf
-sudo grub-mkfont --verbose --range=0x0-0x7F --size=10 --output=/boot/grub/fonts/iosevka-10.pf2 /usr/share/fonts/truetype/iosevka/IosevkaNerdFont-Regular.ttf
-sudo grub-mkfont --verbose --range=0x0-0x7F --size=12 --output=/boot/grub/fonts/iosevka-12.pf2 /usr/share/fonts/truetype/iosevka/IosevkaNerdFont-Regular.ttf
-sudo echo 'GRUB_THEME=/boot/grub/themes/GruvGRUB/GruvGRUB' >> /etc/default/grub && sudo update-grub
+sudo chown -R root:root GruvGRUB &
+sudo mkdir -p /boot/grub/themes/GruvGRUB/ && sudo mv GruvGRUB /boot/grub/themes/GruvGRUB/ &
+sudo grub-mkfont --verbose --range=0x0-0x7F --size=8 --output=/boot/grub/fonts/iosevka-8.pf2 /usr/share/fonts/truetype/iosevka/IosevkaNerdFont-Regular.ttf &
+sudo grub-mkfont --verbose --range=0x0-0x7F --size=10 --output=/boot/grub/fonts/iosevka-10.pf2 /usr/share/fonts/truetype/iosevka/IosevkaNerdFont-Regular.ttf &
+sudo grub-mkfont --verbose --range=0x0-0x7F --size=12 --output=/boot/grub/fonts/iosevka-12.pf2 /usr/share/fonts/truetype/iosevka/IosevkaNerdFont-Regular.ttf &
+echo "GRUB_THEME=/boot/grub/themes/GruvGRUB/GruvGRUB" | sudo tee -a /etc/default/grub && sudo update-grub &
+echo "GruvGRUB successfully installed." && return 0
